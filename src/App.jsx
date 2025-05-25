91d0f692c145009a1cdaedb336978b4b8777c9a2
Ahmed Ramadan <ahmedramadan8050@gmail.com>
2025-05-26 02:28:00 +0300

    Handle Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,57 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route ,useLocation} from 'react-router';
 import MainPage from './pages/MainPage';
-import MainCards from './components/MainCards';
-
-
 import MovieDetails from './pages/MovieDetails';
 import TvShows from './pages/TvShows';
-// search-feature imports
-
-// import SearchContext from './context/searchContext';
-// import SearchKeyContext from './context/searchKeyContext'
-// import Search from './components/Search';
-// import ResultsPage from './pages/ResultsPage';
-// import { useState } from 'react';
+import SearchContext from './context/searchContext';
+import SearchKeyContext from './context/searchKeyContext'
+import Search from './components/Search';
+import ResultsPage from './pages/ResultsPage';
+import { useState } from 'react';
 
 
 function App() {
   
-  //Don't Remove these comments please 
- 
-  //const [search, setSearch] = useState();
-//  const [searchKeyword, setSearchKeyword] = useState();
+  const [search, setSearch] = useState();
+ const [searchKeyword, setSearchKeyword] = useState();
   return (
      <BrowserRouter>
-      <Navbar />
-       <div className="container my-5">
-   //     <SearchContext.Provider value={{search, setSearch}}>
-    //    <SearchKeyContext.Provider value={{searchKeyword,setSearchKeyword}}>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-    //    <Route path="/search-results" element={<ResultsPage></ResultsPage>} />
+       <SearchContext.Provider value={{search, setSearch}}>
+       <SearchKeyContext.Provider value={{searchKeyword,setSearchKeyword}}>
+        <HandleRouting/>
+     </SearchKeyContext.Provider>
+        </SearchContext.Provider>
+     </BrowserRouter>
+  )
+}
 
+function HandleRouting(){
+const location=useLocation()
+console.log(location.pathname);
+  return (
+  <>
+    <Navbar/>
+       <div className="container my-5">
+
+        //search component  will appear only here 
+        {
+         (location.pathname === "/"
+            || location.pathname === "/tv-shows"
+          )
+            &&
+      <Search/>
+        }
+         <Routes>
+        <Route path="/" element={<MainPage />} />
+       <Route path="/search-results" element={<ResultsPage></ResultsPage>} />
         <Route path="/movie-details/:id" element={<MovieDetails />} />
         <Route path="/tv-shows" element={<TvShows />} />
 
           </Routes>
-    //  </SearchKeyContext.Provider>
-    //    </SearchContext.Provider>
       </div>
-     </BrowserRouter>
-  )
+
+  </>);
 }
 
 export default App
